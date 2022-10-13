e91979af1207c12908a887bd0da3d2bc36c9d421
TMN <super.rikunen@gmail.com>
2022-10-13 18:01:46 +0300

    Final Commit

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from .chain2 import *
+from .Chain import *
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='chain',
+      version='0.1',
+      url='https://github.com/TeeMan508/SiriusPythonBasics',
+      author='TMN',
+      packages=find_packages(exclude=['tests']),
+      zip_safe=False)
